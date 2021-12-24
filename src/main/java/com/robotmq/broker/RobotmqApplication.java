f35a62261e38e8c6e4cdfea495dba47d82833854
ryigitkader <ryigitkader@gmail.com>
2021-12-24 17:32:10 +0300

    initial

---
+++ src/main/java/com/robotmq/broker/RobotmqApplication.java
@@ -0,0 +1,13 @@
+package com.robotmq.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotmqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/robotmq/broker/RobotmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotmq.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
